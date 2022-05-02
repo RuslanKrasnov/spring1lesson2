def08b8ebda75aa112debc32c93c66263798ead0
Ruslan <krasnovrg@gmail.com>
2022-05-02 20:30:00 +0500

    first commit

---
+++ src/main/java/ru/geekbrains/less2/AppConfig.java
@@ -0,0 +1,27 @@
+package ru.geekbrains.less2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.HashMap;
+
+@Configuration
+@ComponentScan("ru.geekbrains.less2")
+public class AppConfig {
+
+    @Bean
+    public FileOutputStream fileOut() throws FileNotFoundException {
+        return new FileOutputStream("1.txt");
+    }
+
+    @Bean
+    @Scope("prototype")
+    public HashMap<Product, Integer> hashMap() {
+        return new HashMap<>();
+    }
+
+}
